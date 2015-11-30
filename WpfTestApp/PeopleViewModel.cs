c7ce160b6523134258e9cdc6eaa783d4b307f9e7
Emrah BAŞKAYA <emrah03@hesido.com>
2015-11-30 15:33:12 +0200

    Accessor cleanup, live filter view added

--- WpfTestApp/PeopleViewModel.cs
+++ WpfTestApp/PeopleViewModel.cs
@@ -13,112 +13,76 @@ namespace WpfTestApp
     class PeopleViewModel : PeopleViewModelEntity
     {
 
-        private WPFCommand _removePersonCommand;
-        private WPFCommand _addPersonCommand;
-        private WPFCommand _filterPeopleCommand;
+        public WPFCommand RemovePersonCommand { get; set; }
+        public WPFCommand AddPersonCommand { get; set; }
+        public WPFCommand FilterPeopleCommand { get; set; }
+        public People CheckSelectedPeople { get; set; }
 
+        public People PeopleList { get; set; }
+        public People SelectedPeople { get; set; }
         public ListCollectionView filteredPeopleView { get; set; }
 
-        public PeopleViewModel()
-        {
-            _removePersonCommand = new WPFCommand(new Action<object> (removePersonAction), x => PeopleList.Count > 0);
-            _addPersonCommand = new WPFCommand(() => { SelectedPerson = PeopleList.addPerson(); Console.WriteLine(CheckSelectedPeople.Count); }, null, false);
-            _filterPeopleCommand = new WPFCommand(new Action(filterSelectedAction), null, false);
-            filteredPeopleView = new ListCollectionView(PeopleList);
-            filteredPeopleView.Filter = (p) => !((Person)p).IsSelected;
-            //_peopleList.CollectionChanged += delegate(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e) { _removePersonCommand.RaiseCanExecuteChanged(); };
-        }
-
-        public WPFCommand RemovePersonCommand
-        {
-            get { return _removePersonCommand; }
-        }
-
-        public WPFCommand AddPersonCommand
-        {
-            get { return _addPersonCommand; }
-        }
-
-        public WPFCommand FilterPeopleCommand
-        {
-            get { return _filterPeopleCommand; }
-        }
-
-        protected People _peopleList = new People();
-
-        public People PeopleList
-        {
-            get { return _peopleList; }
-           // set { _peopleList = value; }
-        }
-
         protected Person _selectedPerson = null;
-
         public Person SelectedPerson
         {
             get { return _selectedPerson; }
-            set {
+            set
+            {
                 if (_selectedPerson != value)
                 {
                     _selectedPerson = value;
                     NotifyPropertyChanged("SelectedPerson");
                 }
             }
-
         }
+
+        public PeopleViewModel()
+        {
+            PeopleList = new People();
+            SelectedPeople = new People();
+            CheckSelectedPeople = new People();
+            RemovePersonCommand = new WPFCommand(new Action<object> (removePersonAction), x => PeopleList.Count > 0);
+            AddPersonCommand = new WPFCommand(() => { SelectedPerson = PeopleList.addPerson(); Console.WriteLine(CheckSelectedPeople.Count); }, null, false);
+            FilterPeopleCommand = new WPFCommand(new Action(filterSelectedAction), null, false);
+            filteredPeopleView = new ListCollectionView(PeopleList);
+            filteredPeopleView.Filter = (p) => !((Person)p).IsSelected;
+
+            filteredPeopleView.LiveFilteringProperties.Add("IsSelected");
+            filteredPeopleView.IsLiveFiltering = true;
+
+            //_peopleList.CollectionChanged += delegate(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e) { _removePersonCommand.RaiseCanExecuteChanged(); };
+        }
+
+
 
         //public Action addPersonAction = PeopleViewModel.addPerson();
 
         public void removeSelectedPersonAction()
         {
-            //if (_selectedPerson != null)
-            //{
             PeopleList.removePerson(SelectedPerson);
             if (PeopleList.Count > 0)
                 SelectedPerson = PeopleList[0];
             else
                 SelectedPerson = null;
-            //}
         }
 
         public void removePersonAction(object toRemove)
         {
-            //if (_selectedPerson != null)
-            //{
-
-            Console.WriteLine((toRemove as Person).Name);
+ //           Console.WriteLine((toRemove as Person).Name);
             var forRemoval = (toRemove as Person);
             int pos = Math.Max(PeopleList.IndexOf(forRemoval), 1);
             PeopleList.removePerson(forRemoval);
             SelectedPerson = (PeopleList.Count > pos - 1) ? PeopleList[pos - 1] : null;
-
         //    RemovePersonCommand.RaiseCanExecuteChanged();
             //}
         }
 
-        private People _selectedPeople = new People();
-
-        public People SelectedPeople
-        {
-            get { return _selectedPeople; }
-            set { _selectedPeople = value; }
-        }
-
-        private People _checkSelectedPeople = new People();
-
-        public People CheckSelectedPeople
-        {
-            get { return _checkSelectedPeople; }
-            set { _checkSelectedPeople = value; }
-        }
-
-
         public void filterSelectedAction()
         {
-           //if(SelectedPerson != null) SelectedPerson.IsSelected = true; //testing notification
-            _checkSelectedPeople = new People(PeopleList.Where((p) => p.IsSelected).ToList());
+            CheckSelectedPeople = new People(PeopleList.Where((p) => p.IsSelected).ToList());
             NotifyPropertyChanged("CheckSelectedPeople");
-            filteredPeopleView.Refresh();//force refresh for view
+            //filteredPeopleView.Refresh();//force refresh for view for when livefiltering is not on
+            Console.WriteLine(PeopleList.Count);
         }
     }
 }
